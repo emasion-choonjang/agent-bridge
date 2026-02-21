c9a5570b71ef46c2cd6714dd467e4465da042551
Kang Dongwan <emasion@kakao.com>
2026-02-21 22:27:47 +0900

    ㅅㅗㄹㅣ ㅊㅜㄱㅏ

---
+++ start-bridge-sori.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# SoRi Agent Bridge Wrapper
+export PATH=/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin
+export HOME=/Users/chunjang-server
+
+# SoRi specific env
+export AGENT_ID=sori
+export REDIS_URL=redis://:choonjang-team-2026@100.123.82.47:6380
+export OPENCLAW_BIN=/opt/homebrew/bin/openclaw
+export TELEGRAM_GROUP=-1003554423969
+export COOLDOWN_MS=5000
+export MAX_DEPTH=3
+
+cd /Users/chunjang-server/dev/personal/agent-bridge
+exec /opt/homebrew/bin/node bridge.js
