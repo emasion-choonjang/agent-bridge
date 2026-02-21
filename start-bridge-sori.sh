59858207909bf8c01034a0f4610bb718d47cd526
Kang Dongwan <emasion@kakao.com>
2026-02-21 22:49:48 +0900

    feat: add start-bridge-sori.sh + gitignore for logs

--- start-bridge-sori.sh
+++ start-bridge-sori.sh
@@ -5,6 +5,10 @@ export HOME=/Users/chunjang-server
 
 # SoRi specific env
 export AGENT_ID=sori
+export AGENT_NAME=sori
+export REDIS_HOST=100.123.82.47
+export REDIS_PORT=6380
+export REDIS_PASSWORD=choonjang-team-2026
 export REDIS_URL=redis://:choonjang-team-2026@100.123.82.47:6380
 export OPENCLAW_BIN=/opt/homebrew/bin/openclaw
 export TELEGRAM_GROUP=-1003554423969
